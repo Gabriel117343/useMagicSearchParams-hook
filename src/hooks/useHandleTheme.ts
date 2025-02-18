daff0eb54b97512124390e8049b6b5621de25d9b
Gabriel S. <gabriel.soliz@inacapmail.cl>
2025-02-17 22:21:21 -0300

    save

--- src/hooks/useHandleTheme.ts
+++ src/hooks/useHandleTheme.ts
@@ -5,7 +5,12 @@ type Theme = 'light' | 'dark'
 export const useHandleTheme = () => {
 
   const defaulValue = useMemo(() => {
-    // Se extrae  una posible configuración de tema del sistema
+    // Intenta obtener el tema del local storage
+    const storedTheme = localStorage.getItem('theme') as Theme | null
+    
+    if (storedTheme === 'dark' || storedTheme === 'light') return storedTheme
+
+    // Si no hay un tema almacenado, se extrae  una posible configuración de tema del sistema
     const actualSistemTheme = window.matchMedia('prefers-color-scheme: dark').matches ? 'dark' : 'light'
     return actualSistemTheme
 
@@ -15,13 +20,15 @@ export const useHandleTheme = () => {
   useEffect(() => {
     if (theme === 'dark') {
       document.querySelector('html').classList.add('dark')
+
     } else {
       document.querySelector('html').classList.remove('dark')
     }
+    localStorage.setItem('theme', theme)
   }, [theme])
-  const onChangeTheme = () => {
+  const handleChangeTheme = () => {
     setTheme(theme === 'light' ? 'dark' : 'light')
   }
 
-  return { theme, onChangeTheme }
+  return { theme, onChangeTheme: handleChangeTheme }
 }
